1e87b6796018e6871c4135ca407fdb6bd8c372b6
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2020-05-07 09:59:35 -0400

    added resource limits

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from sandbox import sandbox_python, prepare_lxc
 
 bot = commands.Bot(command_prefix='$')
 
+IGNORED_ERROR = '''Traceback (most recent call last):
+  File "/usr/lib/python3.8/logging/__init__.py", line 260, in _after_at_fork_child_reinit_locks
+  File "/usr/lib/python3.8/logging/__init__.py", line 228, in _releaseLock
+RuntimeError: cannot release un-acquired lock'''
+
+@bot.event
+async def on_ready():
+    print('Bot is ready.')
+    prepare_lxc()
 
 @bot.command()
 async def py(ctx):
@@ -14,11 +23,12 @@ async def py(ctx):
     start = '```py'
     end = '```'
     code = args[args.find(start)+len(start):args.rfind(end)]
+    print('Running code: ', code)
     def run_code():
         exec(code)
     results = sandbox_python(code)
     await ctx.send('Output: ```' + results['output'] + '\n```')
-    if results['errors']:
+    if results['errors'] != '' and IGNORED_ERROR not in results['errors']:
         await ctx.send('Errors: ```' + results['errors'] + '\n```')
     prepare_lxc()
 
--- sandbox.py
+++ sandbox.py
@@ -13,44 +13,31 @@ def sandbox_python(code):
     # Setup the container object
     c = lxc.Container(CONTAINER_NAME)
     if not c.defined:
-        if not c.create("download", lxc.LXC_CREATE_QUIET, {"dist": "ubuntu",
-                                                           "release": "trusty",
-                                                           "arch": "amd64"}):
-            print("Failed to create the container rootfs", file=sys.stderr)
-            return
-        # Start the container
-        if not c.start():
-            print("Failed to start the container", file=sys.stderr)
-            return
+        prepare_lxc()
+        c = lxc.Container(CONTAINER_NAME)
 
     # Query some information
     print("Container state: %s" % c.state)
     print("Container PID: %s" % c.init_pid)
+    c.set_config_item('lxc.prlimit.cpu', '1')
+
     with open(OUTPUT_FILE, 'w') as output_file, open(ERROR_FILE, 'w') as error_file:
         c.attach_wait(user_code, stdout=output_file, stderr=error_file)
 
-    # Stop the container
-    if not c.stop():
-        print("Failed to kill the container", file=sys.stderr)
-        return
-
-    # Destroy the container
-    if not c.destroy():
-        print("Failed to destroy the container.", file=sys.stderr)
-        return
-
+    stop_and_destroy()
 
     with open(OUTPUT_FILE, 'r') as output_file, open(ERROR_FILE, 'r') as error_file:
         results = {'output': output_file.read(), 'errors': error_file.read()}
     return results
 
 def prepare_lxc():
-    # Create a new container
+    stop_and_destroy()
+
     c = lxc.Container(CONTAINER_NAME)
 
     # Create the container rootfs
-    if not c.create("download", lxc.LXC_CREATE_QUIET, {"dist": "ubuntu",
-                                                       "release": "trusty",
+    if not c.create("download", lxc.LXC_CREATE_QUIET, {"dist": "alpine",
+                                                       "release": "3.10",
                                                        "arch": "amd64"}):
         print("Failed to create the container rootfs", file=sys.stderr)
         return
@@ -59,3 +46,18 @@ def prepare_lxc():
     if not c.start():
         print("Failed to start the container", file=sys.stderr)
         return
+    c.set_config_item('lxc.ephemeral', '1')
+    c.set_config_item('lxc.prlimit.as', '128000000')
+
+def stop_and_destroy():
+    c = lxc.Container(CONTAINER_NAME)
+    if c.defined:
+        # Stop the container
+        if not c.stop():
+            print("Failed to kill the container", file=sys.stderr)
+            return
+
+        # Destroy the container
+        if not c.destroy():
+            print("Failed to destroy the container.", file=sys.stderr)
+            return
