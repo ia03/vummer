4801441f532c55aa353581610d4010d6e7a26ae8
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2020-05-08 13:36:17 -0400

    fixed cloning

--- sandbox.py
+++ sandbox.py
@@ -72,11 +72,12 @@ def stop_and_destroy(container_name):
 
 def setup_base():
     # Create the container rootfs
-    if not base.create("download", lxc.LXC_CREATE_QUIET, {"dist": "ubuntu",
-                                                       "release": "trusty",
-                                                       "arch": "amd64"}):
-        print("Failed to create the container rootfs", file=sys.stderr)
-        return
+    if not base.defined:
+        if not base.create("download", lxc.LXC_CREATE_QUIET, {"dist": "ubuntu",
+                                                           "release": "trusty",
+                                                           "arch": "amd64"}):
+            print("Failed to create the container rootfs", file=sys.stderr)
+            return
 
 
     base.set_config_item('lxc.ephemeral', '1')
