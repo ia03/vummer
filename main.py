9a6f442657ffb78c4fe98ae85be8419c42e6e0ed
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2020-05-09 03:59:48 -0400

    Removed print message

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ async def check_print_queue():
         return
     channel_id = data[0]
     message = escape_mentions(data[1])
-    print('New message to channel', str(channel_id) + ':', str(message))
     if len(message) > 2000:
         await bot.get_channel(channel_id).send(
         '[This message is too large.]')
