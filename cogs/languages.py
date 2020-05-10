d589974f12140b68f67e46d4bc86105054cfe38a
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2020-05-10 01:28:34 -0400

    added more commands

--- cogs/languages.py
+++ cogs/languages.py
@@ -79,112 +79,217 @@ class Languages(commands.Cog):
 
     @commands.command()
     async def py(self, ctx):
-        """Runs Python 3 code. Accepts codeblocks and regular text.
+        """Runs Python (3.8.1) code.
         Usage: $py (code)
         """
         await self.code_command(ctx, 71)
 
     @commands.command()
     async def py2(self, ctx):
-        """Runs Python 2 code. Accepts codeblocks and regular text.
+        """Runs Python (2.7.17) code.
         Usage: $py2 (code)
         """
         await self.code_command(ctx, 70)
 
     @commands.command()
     async def cpp(self, ctx):
-        """Runs C++ code. Accepts codeblocks and regular text.
+        """Runs C++ (GCC 9.2.0) code.
         Usage: $cpp (code)
         """
         await self.code_command(ctx, 54)
 
     @commands.command()
     async def c(self, ctx):
-        """Runs C code. Accepts codeblocks and regular text.
+        """Runs C (GCC 9.2.0) code.
         Usage: $c (code)
         """
         await self.code_command(ctx, 50)
 
     @commands.command()
     async def cs(self, ctx):
-        """Runs C# code. Accepts codeblocks and regular text.
+        """Runs C# (Mono 6.6.0.161) code.
         Usage: $cs (code)
         """
         await self.code_command(ctx, 51)
 
     @commands.command()
     async def oc(self, ctx):
-        """Runs Objective C code. Accepts codeblocks and regular text.
+        """Runs Objective C (Clang 7.0.1) code.
         Usage: $oc (code)
         """
         await self.code_command(ctx, 79)
 
     @commands.command()
     async def java(self, ctx):
-        """Runs Java code. Accepts codeblocks and regular text.
+        """Runs Java (OpenJDK 13.0.1) code.
         Usage: $java (code)
         """
         await self.code_command(ctx, 62)
 
     @commands.command()
     async def js(self, ctx):
-        """Runs Javascript code. Accepts codeblocks and regular text.
+        """Runs JavaScript (Node.js 12.14.0) code.
         Usage: $js (code)
         """
         await self.code_command(ctx, 63)
 
     @commands.command()
     async def sql(self, ctx):
-        """Runs SQL code. Accepts codeblocks and regular text.
+        """Runs SQL (SQLite 3.27.2) code.
         Usage: $sql (code)
         """
         await self.code_command(ctx, 82)
 
     @commands.command()
     async def vb(self, ctx):
-        """Runs Visual Basic .NET code. Accepts codeblocks and regular text.
+        """Runs Visual Basic .NET (vbnc 0.0.0.5943) code.
         Usage: $vb (code)
         """
         await self.code_command(ctx, 84)
 
     @commands.command()
     async def octave(self, ctx):
-        """Runs Octave code. Accepts codeblocks and regular text.
+        """Runs Octave (5.1.0) code.
         Usage: $octave (code)
         """
         await self.code_command(ctx, 66)
 
     @commands.command()
     async def clisp(self, ctx):
-        """Runs Common LISP code. Accepts codeblocks and regular text.
+        """Runs Common LISP (SBCL 2.0.0) code.
         Usage: $clisp (code)
         """
         await self.code_command(ctx, 55)
 
     @commands.command()
     async def ass(self, ctx):
-        """Runs Assembly code. Accepts codeblocks and regular text.
+        """Runs Assembly (NASM 2.14.02) code.
         Usage: $ass (code)
         """
         await self.code_command(ctx, 45)
 
     @commands.command()
     async def bash(self, ctx):
-        """Runs Bash code. Accepts codeblocks and regular text.
+        """Runs Bash (5.0.0) code.
         Usage: $bash (code)
         """
         await self.code_command(ctx, 46)
 
     @commands.command()
     async def php(self, ctx):
-        """Runs PHP code. Accepts codeblocks and regular text.
+        """Runs PHP (7.4.1) code.
         Usage: $php (code)
         """
         await self.code_command(ctx, 68)
 
     @commands.command()
     async def lua(self, ctx):
-        """Runs Lua code. Accepts codeblocks and regular text.
+        """Runs Lua (5.3.5) code.
         Usage: $lua (code)
         """
         await self.code_command(ctx, 64)
+
+    @commands.command()
+    async def pascal(self, ctx):
+        """Runs Pascal (FPC 3.0.4) code.
+        Usage: $pascal (code)
+        """
+        await self.code_command(ctx, 67)
+
+    @commands.command()
+    async def scala(self, ctx):
+        """Runs Scala (2.13.2) code.
+        Usage: $scala (code)
+        """
+        await self.code_command(ctx, 81)
+
+    @commands.command()
+    async def swift(self, ctx):
+        """Runs Swift (5.2.3) code.
+        Usage: $swift (code)
+        """
+        await self.code_command(ctx, 83)
+
+    @commands.command()
+    async def rust(self, ctx):
+        """Runs Rust (1.40.0) code.
+        Usage: $rust (code)
+        """
+        await self.code_command(ctx, 73)
+
+    @commands.command()
+    async def go(self, ctx):
+        """Runs Go (1.13.5) code.
+        Usage: $go (code)
+        """
+        await self.code_command(ctx, 60)
+
+    @commands.command()
+    async def ts(self, ctx):
+        """Runs TypeScript (3.7.4) code.
+        Usage: $ts (code)
+        """
+        await self.code_command(ctx, 74)
+
+    @commands.command()
+    async def kotlin(self, ctx):
+        """Runs Kotlin (1.3.70) code.
+        Usage: $kotlin (code)
+        """
+        await self.code_command(ctx, 78)
+
+    @commands.command()
+    async def rb(self, ctx):
+        """Runs Ruby (2.7.0) code.
+        Usage: $rb (code)
+        """
+        await self.code_command(ctx, 72)
+
+    @commands.command()
+    async def haskell(self, ctx):
+        """Runs Haskell (GHC 8.8.1) code.
+        Usage: $haskell (code)
+        """
+        await self.code_command(ctx, 61)
+
+    @commands.command()
+    async def basic(self, ctx):
+        """Runs Basic (FBC 1.07.1) code.
+        Usage: $basic (code)
+        """
+        await self.code_command(ctx, 47)
+
+    @commands.command()
+    async def fortran(self, ctx):
+        """Runs Fortran (GFortran 9.2.0) code.
+        Usage: $fortran (code)
+        """
+        await self.code_command(ctx, 59)
+
+    @commands.command()
+    async def r(self, ctx):
+        """Runs R (4.0.0) code.
+        Usage: $r (code)
+        """
+        await self.code_command(ctx, 80)
+
+    @commands.command()
+    async def erlang(self, ctx):
+        """Runs Erlang (OTP 22.2) code.
+        Usage: $erlang (code)
+        """
+        await self.code_command(ctx, 58)
+
+    @commands.command()
+    async def cobol(self, ctx):
+        """Runs COBOL (GnuCOBOL 2.2) code.
+        Usage: $cobol (code)
+        """
+        await self.code_command(ctx, 77)
+
+    @commands.command()
+    async def d(self, ctx):
+        """Runs D (DMD 2.089.1) code.
+        Usage: $d (code)
+        """
+        await self.code_command(ctx, 56)
